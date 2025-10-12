c4fc136220d99ca393b8036492e6ac6df3055c5b
H.$ <het.solanki721@gmail.com>
2025-10-12 11:51:44 +0530

    loggin up the origin header

--- server/API/server.js
+++ server/API/server.js
@@ -29,6 +29,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  console.log(req.headers.origin);
+  next();
+});
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
